838aea65856dd16dcd8e89a0063518c8fcd77d51
Muhammad Sounkor <mohsonkor2001@gmail.com>
2025-07-16 13:26:06 +0300

    cleaned gold layer initialization from testing code snippets

--- scripts/Gold Layer/gold_layer_initialization.sql
+++ scripts/Gold Layer/gold_layer_initialization.sql
@@ -93,25 +93,3 @@ LEFT JOIN gold.dim_products pr
 LEFT JOIN gold.dim_customers cu
     ON sd.sls_cust_id = cu.customer_id;
 GO
-
-SELECT 
-    customer_key,
-    COUNT(*) AS duplicate_count
-FROM gold.dim_customers
-GROUP BY customer_key
-HAVING COUNT(*) > 1;
-
-SELECT 
-    product_key,
-    COUNT(*) AS duplicate_count
-FROM gold.dim_products
-GROUP BY product_key
-HAVING COUNT(*) > 1;
-
-SELECT COUNT (*)
-FROM gold.fact_sales f
-LEFT JOIN gold.dim_customers c
-ON c.customer_key = f.customer_key
-LEFT JOIN gold.dim_products p
-ON p.product_key = f.product_key
-WHERE p.product_key IS NULL OR c.customer_key IS NULL  
